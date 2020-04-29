67c0fdfa6a8d80862a88ac62db7c099d44c9e08e
Rohit Nimkar <nehalnimkar@gmail.com>
2020-04-29 20:03:02 +0530

    added python solutions for linked lists

---
+++ linked_list/print_elements.py
@@ -0,0 +1,39 @@
+class SinglyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+
+def printLinkedList(head):
+    curr = head
+    while curr is not None:
+        print(curr.data)
+        curr = curr.next
+
+
+if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
+
